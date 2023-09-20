b48e4333aa7a9ddd6f8288c537dc86fb54631145
劳内 <3073561696@qq.com>
2023-09-20 12:18:50 +0000

    库函数模拟实现
    Signed-off-by: 劳内 <3073561696@qq.com>

--- C语言学习/tetst-9-11/test.c
+++ C语言学习/tetst-9-11/test.c
@@ -116,3 +116,181 @@ int main()
 //
 //	return 0;
 //}
+
+
+#define _CRT_SECURE_NO_WARNINGS 1
+//strcmp模拟实现
+
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	assert(str1&& str2);
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+			return 0;
+		str1++;
+		str2++;
+	}
+	return *str1-*str2;
+}
+int main()
+{
+	char arr1[] = "abcde";
+	char arr2[] = "abcdf";
+	int ret = strcmp(arr1, arr2);
+	printf("%d", ret);
+
+	return 0;
+}
+
+
+//strstr，模拟实现
+#include<stdio.h>
+#include<string.h>
+const char* my_strstr(const char* str1, const char* str2)
+{
+	const char* s1 = str1;
+	const char* s2 = str2;
+	const char* p = str1;
+	if (! *str2)
+	{
+		return str1;
+	}
+	while (*p)
+	{
+		s1 = p;
+		s2 = str2;
+		while (*s1 && *s2&&!(*s1-*s2))//s1,s2遇到\0停止，*s1和*s2不同也停止
+		{
+			s1++;
+			s2++;
+		}
+		if (!*s2)//遇到\0
+			return p;
+		p++;//没找到，可能的初始位置++
+	}
+	return NULL;
+}
+int main()
+{
+	char arr1[] = "abbcdefg";
+	char arr2[] = "bcd";
+	char* cp=my_strstr(arr1, arr2);
+	strncpy(cp, "lll",3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//模拟实现strncpy
+#include<stdio.h>
+#include<assert.h>
+char* my_strncpy(char* destination, const char* source, size_t num)
+{
+	assert(destination && source);
+	int i = 0;
+	char* ret = destination;
+	for (i = 0; i < num&&source[i]!='\0'; i++)  //既要小于num，也要在source的范围内
+	{
+		*destination = *source;
+		destination++;
+		source++;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[] = "abcde";
+	char arr2[20] ;//arr2的空间要足够大
+	char* p = my_strncpy(arr2, arr1,sizeof(arr1));
+	printf("%s", arr2);
+
+	return 0;
+}
+
+//模拟实现strncat
+#include<stdio.h>
+#include<assert.h>
+char* my_strncat(char* destination, const char* source, size_t num)
+{
+	assert(destination && source);
+	char* ret = destination;
+	int i = 0;
+	while (*destination)
+	{
+		destination++;
+	}
+	for (i = 0; i < num; i++)
+	{
+	    *destination = *source;
+		destination++;
+		source++;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "abcd";
+	char arr2[] = "efg";
+	char* p = my_strncat(arr1, arr2,2);
+	printf("%s", p);
+
+	return 0;
+}
+
+
+//模拟实现memcpy --- ***
+#include<stdio.h>
+#include<assert.h>
+void* my_memcpy(void* destination, const void* source, size_t num)
+{
+	void* ret = destination;
+	assert(destination && source);
+	while (num--)
+	{
+		*(char*)destination = *(char*)source;
+		destination = (char*)destination + 1;
+		source = (char*)source + 1;
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7 };
+	int arr2[10] = { 0 };
+	my_memcpy(arr2, arr1, 20);
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", arr2[i]);
+	}
+	return 0;
+}
+
+
+//判断是否是小写字母
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	char a = 'a';
+	char B = 'B';
+	int ret_a = islower(a);
+	int ret_B = islower(B);
+	printf("%d %d", ret_a, ret_B);
+	return 0;
+}
+
+toupper
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	char a = 'a';
+	char A = toupper(a);
+	printf("%c", A);
+
+
+	return 0;
+}
